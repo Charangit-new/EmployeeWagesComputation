b8b6bf84bca52e5e364293a6edf4b03c0f55413a
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-09 19:37:18 +0530

    Added : Switch case Statement

--- employeeWage.sh
+++ employeeWage.sh
@@ -5,15 +5,12 @@ read -p "Enter wage per hour(can be assumed as 20) " wg
 read -p "Enter full day hour(can be assumed as 8) " f
 read -p "Enter part time hour (can be assumed as 4)" h
 
-hoursPerDay=0
 randomCheck=$(( RANDOM%3 ))
-
-if [ $randomCheck -eq 1 ]
-then hoursPerDay=$f
-elif [ $randomCheck -eq 2 ] 
-then hoursPerDay=$h
-else echo "Employee is absent"
-fi
+case $randomCheck in
+        1)  hoursPerDay=$f ;;
+        2)  hoursPerDay=$h ;;
+        *)  hoursPerDay=0 ;;
+        esac
 
 dailyWage=$(( wg * hoursPerDay ))
 echo "DailyWage=$dailyWage"
