236b7ef89a93bd82de01d13635717d7050ae78d8
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-09 20:09:10 +0530

    Added: Condition for calculating wages

--- employeeWage.sh
+++ employeeWage.sh
@@ -4,17 +4,25 @@ read -p "Enter wage per hour(can be assumed as 20) " wg
 read -p "Enter full day hour(can be assumed as 8) " f
 read -p "Enter part time hour (can be assumed as 4)" h
 read -p "Enter no of working days in month (can be assumed as 20) " d
-i=0
-while [ ! $i -ge $d ]
+read -p "Enter no of working hours (can be assumed as 100)" w
+
+br=1
+while [ $br -eq 1 ]
 do
-randomCheck=$(( RANDOM%3 ))
 case $randomCheck in
         1)  hoursPerDay=$f ;;
         2)  hoursPerDay=$h ;;
         *)  hoursPerDay=0  ;;
         esac
-dailyWage=$(( wg * hoursPerDay ))
-totalWage=$(( totalWage + dailyWage ))
-((i++))
+
+	if [[ $workingHours -eq $w ]] || [[ $days -eq $d ]]
+        then br=0 break
+	else
+	randomCheck=$(( RANDOM%3 ))
+	dailyWage=$(( wg * hoursPerDay ))
+	totalWage=$(( totalWage + dailyWage ))
+	workingHours=$(( workingHours + hoursPerDay ))
+	((days++))
+	fi
 done
 echo "Total Wage = $totalWage"
