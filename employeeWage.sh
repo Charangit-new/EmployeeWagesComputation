7086e2843eb481485005b82c4546fffa859970e9
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-09 20:29:32 +0530

    Added : Store Day along with Daily Wage and Total Wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -14,7 +14,7 @@ case $1 in
         *)  hoursPerDay=0  ;;
         esac
 }
-echo "Daily Wage          Total Wage"
+echo "Days         Daily Wage       Total Wage"
 while [ $br -eq 1 ]
 do
 	if [[ $workingHours -eq $w ]] || [[ $days -eq $d ]]
@@ -26,6 +26,6 @@ do
 	workingHours=$(( workingHours + hoursPerDay ))
 	((days++))
 	fi
-	echo "  $dailyWage        $totalWage"
+	echo "$days       $dailyWage        $totalWage"
 done
 
