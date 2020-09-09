a0da5046a4dc85e88cd20f9ad5736062726cf074
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-09 19:51:07 +0530

    Added : Calculating Wages for a Month

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,16 +1,20 @@
 echo "Welcome to Employee Wage Computation Program"
-
 echo "Daily wage of Employee"
 read -p "Enter wage per hour(can be assumed as 20) " wg
 read -p "Enter full day hour(can be assumed as 8) " f
 read -p "Enter part time hour (can be assumed as 4)" h
-
+read -p "Enter no of working days in month (can be assumed as 20) " d
+i=0
+while [ ! $i -ge $d ]
+do
 randomCheck=$(( RANDOM%3 ))
 case $randomCheck in
         1)  hoursPerDay=$f ;;
         2)  hoursPerDay=$h ;;
-        *)  hoursPerDay=0 ;;
+        *)  hoursPerDay=0  ;;
         esac
-
 dailyWage=$(( wg * hoursPerDay ))
-echo "DailyWage=$dailyWage"
+totalWage=$(( totalWage + dailyWage ))
+((i++))
+done
+echo "Total Wage = $totalWage"
