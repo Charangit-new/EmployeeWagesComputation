9b7a7e878ce755173710631ac52f2e726d7340c2
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-09 18:11:31 +0530

    Added: Part time

--- employeeWage.sh
+++ employeeWage.sh
@@ -8,10 +8,10 @@ else
         echo "Not present"
 fi
 
-
 echo "Daily wage of Employee"
 read -p "Enter wage per hour(can be assumed as 20) " wg
 read -p "Enter full day hour(can be assumed as 8) " f
+read -p "Enter part time hour (can be assumed as 4)" h
 
 dailyWage=$(( wg * f))
 echo "DailyWage=$dailyWage"
